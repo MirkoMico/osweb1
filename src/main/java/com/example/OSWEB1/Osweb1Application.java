39ef687e7f7191d85c937b722f9d44b5329749a0
MirkoMico <mirkomicocci@gmail.com>
2024-02-16 14:28:32 +0100

    first commit

---
+++ src/main/java/com/example/OSWEB1/Osweb1Application.java
@@ -0,0 +1,13 @@
+package com.example.OSWEB1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Osweb1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Osweb1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/OSWEB1/Osweb1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.OSWEB1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Osweb1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
